259a6fd674bfae824f0a1a0833260a346ba4eddb
chamanbetra <chaman.betrabet@hotmail.com>
2024-01-22 20:02:07 -0500

    <docs>(adding comments (Time-Space Complexity and approach):ContainsDuplicateSolution

--- chaman/contains_duplicate.py
+++ chaman/contains_duplicate.py
@@ -1,3 +1,11 @@
+""" #Time Complexity -> O(n)
+#Space Complexity -> O(n)
+#Approach:
+1. Create a Hashmap/Dictionary with key value pairs
+2. Check for values having count more than 1 using Counter library
+3. If any item has count > 1, then return True
+4. Else, return False
+ """
 from collections import Counter
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
