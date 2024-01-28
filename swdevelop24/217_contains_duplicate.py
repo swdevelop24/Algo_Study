0b103928983c06e5d192dcd2108cee257ec90fc1
swdevelop24 <swdevelop24@gmail.com>
2024-01-28 15:47:39 -0600

    feat: Solve_217_contains duplicate

---
+++ swdevelop24/217_contains_duplicate.py
@@ -0,0 +1,14 @@
+''' 
+Time Complexity -> O(1)
+#Space Complexity -> O(1) ?
+#Approach:
+1. Create a set from an existing list
+2. Check if the the length of set and lists are the same
+3. If different, duplicate number(s) exist(s) => returns true 
+'''
+
+from collections import defaultdict 
+
+class Solution(object):
+    def containsDuplicate(self, nums):
+        return len(set(nums))!= len(nums)
