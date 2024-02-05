4ae3ec11fa0f69aba80b4e5a32952f7a97f14b4f
swdevelop24 <swdevelop24@gmail.com>
2024-02-04 20:32:30 -0600

    fix: Update .gitignore - add solutions folder

---
+++ swdevelop24/solutions/217_contains_duplicate.py
@@ -0,0 +1,23 @@
+"""
+1) BRUTE FORCE : check one vs the next one : time - O(N^2), space - O(1)-no extra memory required
+2) SORTING & Check adjacent values :  1 2 3 1 => 1 1 2 3 : time O(NlogN), space - O(1)
+3) HASHSET: Does this exist in your hashset, if not add to the hashset? 1,2,3,1 
+   time- O(N) & space O(N)
+"""
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+
+        return False
+
+
+sol = Solution()
+print(sol.containsDuplicate([1, 2, 3, 1]))
+print(Solution.containsDuplicate(Solution, [1, 2, 3, 4]))
---
+++ swdevelop24/solutions/242_valid_anagram.py
@@ -0,0 +1,56 @@
+# Neetcode
+"""
+what is anagram?  made up of the exact list of characters
+approach: count the occrances of the characters in both strings => hashmap
+
+time: O(S+T)
+space: O(S+T) => b/c we are building a hashmap 
+downside: wea are building an extra memory
+
+"""
+
+
+# 1) hashmap solution
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            # key error will occur if wirtten like this
+            # countS[s[i]] = 1 + countS[s[i]]
+            # to avoid that => use get method with second parameter-> default value:0
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+
+        return True
+
+
+print(Solution.isAnagram(Solution, "anagram", "nagaram"))
+
+# 2) Counter library (not that recommended for an interview)
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+
+print(Solution.isAnagram(Solution, "anagram", "nagaram"))
+
+
+# 3) sortecd order : time complexity => nlogn //
+# space complexity : usually o(1) during the interview  you need to discuss
+# not as efficent as the above two solutions
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+
+print(Solution.isAnagram(Solution, "anagram", "nagaram"))
